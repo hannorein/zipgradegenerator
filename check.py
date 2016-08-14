0a3911cfc0895715412290bebadad137812e617d
Hanno Rein <hanno@hanno-rein.de>
2016-08-14 15:46:55 -0400

    more checks

--- check.py
+++ check.py
@@ -6,24 +6,26 @@ def run_student_code(filename,return_dict):
     output = subprocess.Popen(["python3", filename], stdout=subprocess.PIPE,stderr=subprocess.STDOUT).communicate()[0].decode("ascii")
     return_dict["output"] = output
 
-with open('correct_output.txt', 'r') as content_file:
-    correct_output = content_file.read()
+with open('correct_output.txt', 'r') as cf:
+    correct_output = cf.read()
 
 manager = Manager()
 return_dict = manager.dict()
 for f in glob.glob("submissions/*.py"):
-    iscorrect = 1
+    notcorrect = None
+    with open(f, 'r') as cf:
+        sf = cf.read()
+    if "import" in sf:
+        notcorrect = "Import statement used"
     sid = f.split("/")[1].split(" ")[0]
     p = Process(target=run_student_code,args=(f,return_dict))
     p.start()
     p.join(1)
     if p.is_alive():
         p.terminate()
-        iscorrect = 0
+        notcorrect = "Execution time too long"
     if return_dict["output"]!=correct_output:
-        iscorrect = 0
-    if iscorrect:
-        print(sid, "correct")
-    else:
-        print(sid, "not correct")
+        notcorrect = "Output not correct"
+    print(sid, ", Not good,", notcorrect)
+
 
