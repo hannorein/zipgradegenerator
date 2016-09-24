70b7711e36825ae66791c5bc3f77dfa2bf4f1c81
Hanno Rein <hanno@hanno-rein.de>
2016-09-24 11:05:33 -0400

    return

--- checkSubmission.py
+++ checkSubmission.py
@@ -19,24 +19,23 @@ manager = Manager()
 return_dict = manager.dict()
 
 def checkSubmission(f):
-    notcorrect = None
     if "output" in return_dict:
         del return_dict["output"]
     with open(f, 'r') as cf:
         sf = cf.readlines()
     if len(sf)>70:
-        notcorrect = "Too many lines of code (max: 70)."
+        return "Too many lines of code (max: 70)."
     with open(f, 'r') as cf:
         sf = cf.read()
     if "import" in sf:
-        notcorrect = "Import statement used"
+        return "Import statement used"
     if "input" in sf:
-        notcorrect = "Input statement used. Do not use the input statement for this assignment."
+        return "Input statement used. Do not use the input statement for this assignment."
     pm = __import__(splitext(basename(f))[0])
     if "f" not in dir(pm):
-        notcorrect = "Function f not defined."
+        return "Function f not defined."
     if "g" not in dir(pm):
-        notcorrect = "Function g not defined."
+        return "Function g not defined."
 
 
     ############
@@ -47,15 +46,15 @@ def checkSubmission(f):
     for l in flines:
         for c in l:
             if c not in allowedchars:
-                notcorrect = "Character not allowed: '"+c+"'."
+                return "Character not allowed: '"+c+"'."
     
     corf = [[1e-16, 0.], [ 2.5e-16,1.],[1e-13,0.9988901220865705],[1.12e-16,2.],[-1e15,1.],[1e-15,1.1111111111111112]]
     for a,b in corf:
         try:
             if pm.f(a)!=b:
-                notcorrect = "Incorrect return value for f(%.16e)."%a
+                return "Incorrect return value for f(%.16e)."%a
         except:
-            notcorrect = "Incorrect return value for f(%.16e)."%a
+            return "Incorrect return value for f(%.16e)."%a
     
     ############
     ############  Test for g(x)
@@ -66,27 +65,27 @@ def checkSubmission(f):
         if "for" in l:
             ffor = 1
     if ffor==0:
-        notcorrect = "Function g contains no for loop."
+        return "Function g contains no for loop."
 
 
     corg = [[1e-300, float("inf")], [0.,0.],[0,0],[-1e-15,float("-inf")],[1e300,float("inf")]]
     for a,b in corg:
         try:
             if pm.g(a)!=b:
-                notcorrect = "Incorrect return value for g(%.16e)."%a
+                return "Incorrect return value for g(%.16e)."%a
         except:
-            notcorrect = "Incorrect return value for g(%.16e)."%a
+            return "Incorrect return value for g(%.16e)."%a
             pass
 
     
     for a in [0,1,1000000,2000,-234234,10000002030403827287872]:
         try:
             if math.isinf(pm.g(a)):
-                notcorrect = "Incorrect return value for g(%d)."%a
+                return "Incorrect return value for g(%d)."%a
         except OverflowError:
             pass
         except:
-            notcorrect = "Incorrect return value for g(%d)."%a
+            return "Incorrect return value for g(%d)."%a
             pass
     
     ############
@@ -96,15 +95,15 @@ def checkSubmission(f):
     for a,b in corfibd:
         try:
             if math.fabs(pm.fibd(a)-b)>max(1e-10,1e-10*b):
-                notcorrect = "Incorrect return value for fibd(%d)."%a
+                return "Incorrect return value for fibd(%d)."%a
         except:
-            notcorrect = "Incorrect return value for fibd(%d)."%a
+            return "Incorrect return value for fibd(%d)."%a
             pass
     try:
         if math.fabs(pm.fibd(90))<1000:
-            notcorrect = "Incorrect return value for fibd(%d)."%90
+            return "Incorrect return value for fibd(%d)."%90
     except:
-        notcorrect = "Incorrect return value for fibd(%d)."%90
+        return "Incorrect return value for fibd(%d)."%90
 
 
     
@@ -113,15 +112,14 @@ def checkSubmission(f):
     p.join(1)
     if p.is_alive():
         os.kill(-return_dict["pid"], signal.SIGTERM)
-        notcorrect = "Runtime too long"
-    if notcorrect is None:
-        o = return_dict["output"]
-        if o !=correct_output:
-            if "SyntaxError" in o:
-                notcorrect = "Syntax error"
-            else:
-                notcorrect = "Output not correct"
-    return notcorrect
+        return "Runtime too long"
+    o = return_dict["output"]
+    if o !=correct_output:
+        if "SyntaxError" in o:
+            return "Syntax error"
+        else:
+            return "Output not correct"
+    return None
 
 if __name__ == "__main__":
     if len(sys.argv)<2:
@@ -131,6 +129,6 @@ if __name__ == "__main__":
     ret = checkSubmission(f)
     if ret:
         print("Test not passed. Reason:")
-        print(checkSubmission(f))
+        print(ret)
     else:
         print("Test passed.")
