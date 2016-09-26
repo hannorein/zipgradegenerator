7e8f215a5d14f609bb884c3401397558a7ff4b4d
Hanno Rein <hanno@hanno-rein.de>
2016-09-26 14:50:48 -0400

    checks for s

--- checkSubmission.py
+++ checkSubmission.py
@@ -26,85 +26,65 @@ def checkSubmission(f):
         sf = cf.readlines()
     if len(sf)>70:
         return "Too many lines of code (max: 70)."
+    for l in sf:
+        if "import" in l:
+            if l.strip() != "from numpy.linalg import solve":
+                return "You may only use the following import statement: from numpy.linalg import solve"
     with open(f, 'r') as cf:
         sf = cf.read()
-    if "import" in sf:
-        return "Import statement used"
     if "input" in sf:
         return "Input statement used. Do not use the input statement for this assignment."
     pm = __import__(splitext(basename(f))[0])
-    if "f" not in dir(pm):
-        return "Function f not defined."
-    if "g" not in dir(pm):
-        return "Function g not defined."
+    if "s" not in dir(pm):
+        return "Function s(A,v) not defined."
+    if "lst" not in dir(pm):
+        return "Function lst(x,y,t) not defined."
 
 
     ############
-    ############  Test for f(x)
+    ############  Test for s()
     ############
-    flines,flinesn = inspect.getsourcelines(pm.f)
-    allowedchars = ["d","e","f"," ","(","x",")",":","\n","r","t","u","n","\t","1","-","+","/"]
-    for l in flines:
-        for c in l:
-            if c not in allowedchars:
-                return "Character not allowed: '"+c+"'."
-    
-    corf = [[1e-16, 0.], [ 2.5e-16,1.],[1e-13,0.9988901220865705],[1.12e-16,2.],[-1e15,1.],[1e-15,1.1111111111111112]]
-    for a,b in corf:
-        try:
-            if pm.f(a)!=b:
-                return "Incorrect return value for f(%.16e)."%a
-        except:
-            return "Incorrect return value for f(%.16e)."%a
-    
-    ############
-    ############  Test for g(x)
-    ############
-    glines,glinesn = inspect.getsourcelines(pm.g)
-    ffor = 0
-    for l in glines:
-        if "for" in l:
-            ffor = 1
-    if ffor==0:
-        return "Function g contains no for loop."
+    A = [[2.,  1.,  4.,  1.],
+         [3.,  4., -1., -1.],
+         [1., -4.,  1.,  5.],
+         [2., -2.,  1.,  3.]]
 
+    v = [-4., 3., 9., 7. ]
+    sol1 = [ 2., -1., -2.,  1.]
 
-    corg = [[1e-300, float("inf")], [0.,0.],[0,0],[-1e-15,float("-inf")],[1e300,float("inf")]]
-    for a,b in corg:
-        try:
-            if pm.g(a)!=b:
-                return "Incorrect return value for g(%.16e)."%a
-        except:
-            return "Incorrect return value for g(%.16e)."%a
-            pass
-
-    
-    for a in [0,1,1000000,2000,-234234,10000002030403827287872]:
-        try:
-            if math.isinf(pm.g(a)):
-                return "Incorrect return value for g(%d)."%a
-        except OverflowError:
-            pass
-        except:
-            return "Incorrect return value for g(%d)."%a
-            pass
-    
-    ############
-    ############  Test for fibd(x)
-    ############
-    corfibd = [[1,0.], [10,1.421085471520200372e-14],[20,4.547473508864641190e-12]]
-    for a,b in corfibd:
-        try:
-            if math.fabs(pm.fibd(a)-b)>max(1e-10,1e-10*b):
-                return "Incorrect return value for fibd(%d)."%a
-        except:
-            return "Incorrect return value for fibd(%d)."%a
-            pass
     try:
-        if math.fabs(pm.fibd(90))<1000:
-            return "Incorrect return value for fibd(%d)."%90
+        sol =  pm.s(A,v)
+        if len(sol) != len(v):
+            return "Incorrect dimensions in return value from s()."
+        for i in range(len(sol)):
+            if abs( (sol[i]-sol1[i]) / sol1[i])>1e-8:
+                return "Function s() does not seem to solve a linear system of equations correctly."
     except:
-        return "Incorrect return value for fibd(%d)."%90
+        return "Something went wrong when executing the function s() to solve a linear system of equations."
+    
+    
+    ############
+    ############  Test for s()
+    ############
+    A = [[0.,  1.,  4.,  1.],
+         [3.,  4., -1., -1.],
+         [1., -4.,  1.,  5.],
+         [2., -2.,  1.,  3.]]
+
+    v = [-4., 3., 9., 7. ]
+    sol1 =[ 1.61904762, -0.42857143, -1.23809524,  1.38095238]
+
+    try:
+        sol =  pm.s(A,v)
+        if len(sol) != len(v):
+            return "Incorrect dimensions in return value from s()."
+        for i in range(len(sol)):
+            if abs( (sol[i]-sol1[i]) / sol1[i])>1e-5:
+                return "Function s() does not seem to solve a linear system of equations correctly."
+    except:
+        return "Something went wrong when executing the function s() to solve a linear system of equations."
+    
+    
 
 
     
